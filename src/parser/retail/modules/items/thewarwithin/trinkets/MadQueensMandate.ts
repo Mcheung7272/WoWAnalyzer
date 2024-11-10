e621a4c648cb68b798f40c5191acb377cb8eb3b8
mcheung7272 <mchung7272@gmail.com>
2024-11-09 23:23:14 -0500

    added logic to reduce cd on kill for queens mandate

--- src/parser/retail/modules/items/thewarwithin/trinkets/MadQueensMandate.ts
+++ src/parser/retail/modules/items/thewarwithin/trinkets/MadQueensMandate.ts
@@ -1,12 +1,17 @@
 import ITEMS from 'common/ITEMS/thewarwithin/trinkets';
 import SPELLS from 'common/SPELLS/thewarwithin/trinkets';
 import Analyzer, { Options } from 'parser/core/Analyzer';
+import Events, { DamageEvent } from 'parser/core/Events';
 import SPELL_CATEGORY from 'parser/core/SPELL_CATEGORY';
 import Abilities from 'parser/core/modules/Abilities';
+import SpellUsable from 'parser/shared/modules/SpellUsable';
 
 export default class MadQueensMandate extends Analyzer.withDependencies({
   abilities: Abilities,
+  spellUsable: SpellUsable,
 }) {
+  protected spellUsable!: SpellUsable;
+
   constructor(options: Options) {
     super(options);
 
@@ -20,5 +25,13 @@ export default class MadQueensMandate extends Analyzer.withDependencies({
       category: SPELL_CATEGORY.COOLDOWNS,
       cooldown: 120,
     });
+
+    this.addEventListener(Events.damage.spell(SPELLS.ABYSSAL_GLUTTONY), this.onDamage);
+  }
+
+  private onDamage(event: DamageEvent) {
+    if (event.overkill && event.overkill > 0) {
+      this.spellUsable.reduceCooldown(SPELLS.ABYSSAL_GLUTTONY.id, 60);
+    }
   }
 }
